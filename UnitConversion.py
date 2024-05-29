5e520997188e6369f8f34061787fb2ae6fc632e5
SachinCloudurity <sachin.kulkarni@cloudurity.com>
2024-05-29 19:10:08 +0530

    Update-3 UnitConversion.py
    the unit conversions performed are now first converted to a base unit(meter, kgs, secs) then to required unit which reduces the coupling between the units of one type, more unit subtypes of a single unit type can be added since each subtype is now independent.

--- UnitConversion.py
+++ UnitConversion.py
@@ -7,26 +7,25 @@ class Conversion(BaseModel):
     unit: str
     convert_to: str
 
-conversions={
-    'length':{'kms','miles'},
-    'weight':{'pounds','kgs'},
-    'time':{'hours','mins'},
-    'temperature':{'far','cel'}
+conversions = {
+    'length': {'kms', 'miles', 'meters'},
+    'weight': {'pounds', 'kgs'},
+    'time': {'hrs', 'mins', 'secs'},
+    'temperature': {'far', 'cel'}
 }
 
 #retrives the available conversions for length,weight,time and temperature as a get method.
-@app.get('/get_convertion/{conversions_type}')
-def convert_length(conversions_type: str=None):
-    return conversions[conversions_type]
+@app.get('/get_conversion/{conversions_type}')
+def get_conversions(conversions_type: str = None):
+    if conversions_type not in conversions:
+        raise HTTPException(status_code=400, detail="Invalid conversion type")
+    return list(conversions[conversions_type])
 
-#static multiplication factors. 
+#static multiplication factors, i have taken meters,kgs and seconds as the base units
 conversion_factors = {
-    "miles_to_kms": 1.60934,
-    "kms_to_miles": 0.621371,
-    "pounds_to_kgs": 0.453592,
-    "kgs_to_pounds": 2.20462,
-    "hrs_to_mins": 60,
-    "mins_to_hrs": 1/60,
+    'length': {'kms': 1000,'miles': 1609.34,'meters': 1 },
+    'weight': {'pounds': 0.453592, 'kgs': 1},
+    'time': {'hrs': 3600,'mins': 60,'secs': 1}
 }
 
 
@@ -40,20 +39,35 @@ def fahrenheit_to_celsius(f: float) -> float:
     if f < -459.67:
         raise HTTPException(status_code=400, detail="Temperature below absolute zero is not possible")
     return (f - 32) * 5/9
-conversion_functions = {
-    "cel_to_far": celsius_to_fahrenheit,
-    "far_to_cel": fahrenheit_to_celsius,
-}
+
+#first function converts every given unit to a bse unit(meter,kgs,secs).
+def tobase_convert(value: float, from_unit: str, unit_type: str) -> float:
+    if unit_type == 'temperature':
+        if from_unit == 'far':
+            return fahrenheit_to_celsius(value)
+        return value
+    else:
+        return value * conversion_factors[unit_type][from_unit]
+#second function converts every base unit to required unit.
+def frombase_convert(value: float, to_unit: str, unit_type: str) -> float:
+    if unit_type == 'temperature':
+        if to_unit == 'far':
+            return celsius_to_fahrenheit(value)
+        return value
+    else:
+        return value / conversion_factors[unit_type][to_unit]
+
 
 # function that takes user input to perform unit conversions based on static or variable multiplication factors.
 def convert(value: float, from_unit: str, to_unit: str) -> float:
-    i = f"{from_unit}_to_{to_unit}"
-    if i in conversion_factors and value>0 :
-        return value * conversion_factors[i]
-    elif i in conversion_functions:
-        return conversion_functions[i](value)
-    else:
-        raise HTTPException(status_code=400, detail=f"Please enter correct conversion unit{conversions}")
+    unit_types = {utype: units for utype, units in conversions.items() if from_unit in units and to_unit in units}
+    if not unit_types:
+        raise HTTPException(status_code=400, detail=f"Invalid conversion units. Available conversions: {conversions}")
+    
+    unit_type = list(unit_types.keys())[0]
+    base_value = convert_to_base(value, from_unit, unit_type)
+    result_value = convert_from_base(base_value, to_unit, unit_type)
+    return result_value
     
 #takes the input to update the results.
 @app.post("/convert")
